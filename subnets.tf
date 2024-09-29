3f363dd0193b0a7d8cc26308f44f79e8766404ed
Hussam Alkhafaji <h-ssom1997@hotmail.com>
2024-09-29 23:36:51 +0200

    only create ipv6 cidrs when it is requested

--- subnets.tf
+++ subnets.tf
@@ -7,12 +7,14 @@ module "ipv4_subnets" {
   networks = [
     for sn in var.sn_details : {
       name     = sn.name
-      new_bits = 4
+      new_bits = var.ipv4_subnets_new_bits
     }
   ]
 }
 
 module "ipv6_subnets" {
+  count = var.is_ipv6_enabled ? 1 : 0
+
   source  = "hashicorp/subnets/cidr"
   version = "1.0.0"
 
@@ -21,7 +23,7 @@ module "ipv6_subnets" {
   networks = [
     for sn in var.sn_details : {
       name     = sn.name
-      new_bits = 8
+      new_bits = var.ipv6_subnets_new_bits
     }
   ]
 }
